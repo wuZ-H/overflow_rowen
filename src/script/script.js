fe9a103f9c825eab4a02e673d9964f717325681d
wuZ-H <129127081+wuZ-H@users.noreply.github.com>
2024-03-27 19:52:51 +0800

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -2,7 +2,11 @@ var counter_rowen=0;
 
 function play_click(){
   create_div(box);
+  set_size(box,"80vh","40vh");
+  set_posi(box,"10vh","calc(50vw-20vh)");
   create_div(bar_top);
+  set_size(bar_top,"10vh","100vw");
+  set_posi(bar_top,"0","0");
   create_rowen();
 }
 
@@ -20,10 +24,10 @@ function create_div(id){
   document.body.appendChild(id);
 }
 function set_size(i,h,w){
-  document.getElementById(i).style.height="calc("+h+")";
-  document.getElementById(i).style.width="calc("+w+")";
+  document.getElementById(i).style.height=h";
+  document.getElementById(i).style.width=w;
 }
 function set_posi(i,t,l){
-  document.getElementById(i).style.top="calc("+t+")";
-  document.getElementById(i).style.left="calc("+l+")";
+  document.getElementById(i).style.top=t;
+  document.getElementById(i).style.left=l;
 }
