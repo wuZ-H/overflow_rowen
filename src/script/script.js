9a7f08fe6368e57790cb52fb6af424233f1e64e1
wuZ-H <129127081+wuZ-H@users.noreply.github.com>
2024-03-27 19:22:15 +0800

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -1,3 +1,12 @@
-function play_start(){
+var counter_rowen=0;
+
+function play_click(){
   
+}
+function create_rowen(){
+  counter_rowen++;
+  var rowen=document.createElement("div");
+  cell.setAttribute("class","rowen");
+  cell.setAttribute("id","rowen"+counter_rowen);
+  document.body.appendChild(rowen);
 }
