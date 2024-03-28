e6a1454638e6d74cf84cc390d20099138523964d
wuZ-H <129127081+wuZ-H@users.noreply.github.com>
2024-03-28 23:30:05 +0800

    Update script.js

--- src/script/script.js
+++ src/script/script.js
@@ -1,16 +1,16 @@
 var counter_rowen=0;
 
 function play_click(){
-  create_div(box);
+  /*create_div(box);
   set_size(box,"80vh","40vh");
   set_posi(box,"10vh","calc(50vw-20vh)");
   create_div(bar_top);
   set_size(bar_top,"10vh","100vw");
   set_posi(bar_top,"0","0");
-  create_rowen();
+  create_rowen();*/
 }
 
-function create_rowen(){
+/*function create_rowen(){
   counter_rowen++;
   var rowen=document.createElement("div");
   rowen.setAttribute("class","rowen");
@@ -36,4 +36,4 @@ function set_size(i,h,w){
 function set_posi(i,t,l){
   document.getElementById(i).style.top=t;
   document.getElementById(i).style.left=l;
-}
+}*/
