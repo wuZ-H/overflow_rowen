902e19e28555c7442f45949ec61a05b6454a54a2
wuZ-H <129127081+wuZ-H@users.noreply.github.com>
2024-03-29 02:23:01 +0800

    Add files via upload

--- src/script/script.js
+++ src/script/script.js
@@ -1,19 +1,23 @@
 var counter_rowen=0;
 
 var btn=document.getElementById("PLAY");
-btn.setAttribute("onclick","play_click");
+btn.setAttribute("onclick","play_click()");
 
 function play_click(){
-  /*create_div(box);
-  set_size(box,"80vh","40vh");
-  set_posi(box,"10vh","calc(50vw-20vh)");
-  create_div(bar_top);
-  set_size(bar_top,"10vh","100vw");
-  set_posi(bar_top,"0","0");
-  create_rowen();*/
+  var p=document.getElementById("title");
+  document.body.removeChild(p);
+  var btn=document.getElementById("PLAY");
+  document.body.removeChild(btn);
+  create_div("box");
+  set_size("box","80vh","60vh");
+  set_posi("box","10vh","calc(50vw - 30vh)");
+  create_div("bar_top");
+  set_size("bar_top","10vh","100vw");
+  set_posi("bar_top","0","0");
+  create_rowen();
 }
 
-/*function create_rowen(){
+function create_rowen(){
   counter_rowen++;
   var rowen=document.createElement("div");
   rowen.setAttribute("class","rowen");
@@ -39,4 +43,4 @@ function set_size(i,h,w){
 function set_posi(i,t,l){
   document.getElementById(i).style.top=t;
   document.getElementById(i).style.left=l;
-}*/
+}
