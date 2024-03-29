600b71e36a3429e062ba1bac98f381d7d1a22b80
wuZ-H <129127081+wuZ-H@users.noreply.github.com>
2024-03-30 06:11:48 +0800

    Add files via upload

--- src/script/script.js
+++ src/script/script.js
@@ -1,4 +1,17 @@
 var counter_rowen=0;
+var timer_drop_rowen;
+var array_rowen=[0];
+array_rowen[0]=[0,0,0,0,0,0,0,0,0,0];
+array_rowen[1]=[0,0,0,0,0,0,0,0,0,0];
+array_rowen[2]=[0,0,0,0,0,0,0,0,0,0];
+array_rowen[3]=[0,0,0,0,0,0,0,0,0,0];
+array_rowen[4]=[0,0,0,0,0,0,0,0,0,0];
+array_rowen[5]=[0,0,0,0,0,0,0,0,0,0];
+array_rowen[6]=[0,0,0,0,0,0,0,0,0,0];
+array_rowen[7]=[0,0,0,0,0,0,0,0,0,0];
+array_rowen[8]=[0,0,0,0,0,0,0,0,0,0];
+array_rowen[9]=[0,0,0,0,0,0,0,0,0,0];
+array_rowen[10]=[0,0,0,0,0,0,0,0,0,0];
 
 var btn=document.getElementById("PLAY");
 btn.setAttribute("onclick","play_click()");
@@ -14,21 +27,58 @@ function play_click(){
   create_div("bar_top");
   set_size("bar_top","10vh","100vw");
   set_posi("bar_top","0","0");
-  create_rowen();
+  for(var i=0; i<10; i++){
+    for(var j=0; j<10; j++){
+      while(array_rowen[][]){
+      }
+      create_rowen(i*10+j,j,0);
+      drop_rowen(10*i+j);
+    }
+  }
 }
 
-function create_rowen(){
+function create_rowen(id,x,y){
   counter_rowen++;
   var rowen=document.createElement("div");
   rowen.setAttribute("class","rowen");
   rowen.setAttribute("id","rowen"+counter_rowen);
   document.body.appendChild(rowen);
+  array_rowen[x][y]="rowen"+id;
+  set_size("rowen"+counter_rowen,"8vh","6vh");
 }
 
 function delete_rowen(id){
-  var rowen=document.getElementById(id);
+  var rowen=document.getElementById("rowen"+id);
   document.body.removeChild(rowen);
   counter_rowen--;
+}
+
+function drop_rowen(id){
+  var rowen=document.getElementById("rowen"+id);
+  var x=0;
+  var x_=0;
+  var y=0;
+  for(var i=0; i<10; i++){
+    x=array_rowen[i].indexOf("rowen"+id);
+    if(x != -1){
+      y=i;
+      x_=x;
+    }
+  }
+  x=x_;
+  drop_rowen_timer(id,x,y);
+}
+
+function drop_rowen_timer(id,x,y){
+  clearTimeout(timer_drop_rowen);
+  timer_drop_rowen=null;
+  if(array_rowen[y+1][x] == 0){
+    array_rowen[y][x]=0;
+    array_rowen[y+1][x]="rowen"+id;
+    set_posi("rowen"+counter_rowen,"calc(10vh + 8vh * "+y+")","calc(50vw - 30vh + 6vh * " + x + ")");
+    y++;
+    timer_drop_rowen=setTimeout(function(){drop_rowen_timer(id,x,y)},500);
+  }
 }
 
 function create_div(id){
